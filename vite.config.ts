343ef24cb8728c8b78c6256794437cc4dbfd4ea3
Caedmon Mullin <caedmon.m@gmail.com>
2025-10-27 10:05:55 +0000

    chore: update package.json for version bump and build script adjustments; add tsconfig.build.json; enhance vite.config.ts for library build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
@@ -12,4 +12,27 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    lib: {
+      entry: path.resolve(
+        __dirname,
+        "src/components/MonthPicker/MonthPicker.tsx"
+      ),
+      name: "SimpleReactMonthPicker",
+      fileName: "index",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      // Exclude peer deps from the bundle
+      external: ["react", "react-dom", "dayjs"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          dayjs: "dayjs",
+        },
+      },
+    },
+    outDir: "dist", // ensure this matches what's in your package.json
+  },
 });
